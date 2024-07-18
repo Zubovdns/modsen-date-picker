af5a7af7e198455c15d8b2eb3b2fbf5de4366fa8
Monika <zubovdns@icloud.com>
2024-07-18 21:32:20 +0300

    feat: added import for task datepicker

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Calendar } from '@components/Calendar';
 export { default as Datepicker } from '@components/Datepicker';
+export { default as RangeDatepicker } from '@components/RangeDatepicker';
+export { default as TaskDatepicker } from '@components/TaskDatepicker';
