2601abe0026d43417a571457c7c1083b4f3544ae
Monika <zubovdns@icloud.com>
2024-07-25 21:38:09 +0300

    test: rewrote test for getDays

--- src/utils/__tests__/getDays.test.ts
+++ src/utils/__tests__/getDays.test.ts
@@ -8,113 +8,113 @@ describe('getDays', () => {
 	const currentDate = new Date(2023, 6, 1);
 
 	test('возвращает правильные дни месяца без дополнительных дней и праздников, начиная с воскресенья', () => {
-		const days = getDays(currentDate, 'sunday', false, false);
+		const days = getDays(currentDate, 'sunday', false, false, false, []);
 		const daysInMonth = 31;
-		expect(days.filter((day) => !day.$isOutsideMonth)).toHaveLength(
-			daysInMonth
-		);
+		expect(days.filter((day) => !day.isOutsideMonth)).toHaveLength(daysInMonth);
 
 		expect(
-			days.find((day) => day.date.getDate() === 1 && !day.$isOutsideMonth)
+			days.find((day) => day.date.getDate() === 1 && !day.isOutsideMonth)
 		).toEqual({
 			key: 'mocked-uuid',
 			day: 1,
 			date: new Date(2023, 6, 1),
-			$isOutsideMonth: false,
-			$holiday: false,
+			isOutsideMonth: false,
+			weekend: false,
+			holiday: false,
 		});
 	});
 
 	test('возвращает правильные дни месяца без дополнительных дней и праздников, начиная с понедельника', () => {
-		const days = getDays(currentDate, 'monday', false, false);
+		const days = getDays(currentDate, 'monday', false, false, false, []);
 		const daysInMonth = 31;
-		expect(days.filter((day) => !day.$isOutsideMonth)).toHaveLength(
-			daysInMonth
-		);
+		expect(days.filter((day) => !day.isOutsideMonth)).toHaveLength(daysInMonth);
 
 		expect(
-			days.find((day) => day.date.getDate() === 1 && !day.$isOutsideMonth)
+			days.find((day) => day.date.getDate() === 1 && !day.isOutsideMonth)
 		).toEqual({
 			key: 'mocked-uuid',
 			day: 1,
 			date: new Date(2023, 6, 1),
-			$isOutsideMonth: false,
-			$holiday: false,
+			isOutsideMonth: false,
+			weekend: false,
+			holiday: false,
 		});
 	});
 
 	test('возвращает правильные дни, включая дополнительные дни из предыдущего и следующего месяца, начиная с воскресенья', () => {
-		const days = getDays(currentDate, 'sunday', true, false);
+		const days = getDays(currentDate, 'sunday', true, false, false, []);
 		expect(days).toHaveLength(42);
 
 		expect(days[0]).toEqual({
 			key: 'mocked-uuid',
 			day: 25,
 			date: new Date(2023, 5, 25),
-			$isOutsideMonth: true,
-			$holiday: false,
+			isOutsideMonth: true,
+			weekend: false,
+			holiday: false,
 		});
 	});
 
 	test('корректно отмечает праздничные дни (воскресенья и субботы)', () => {
-		const days = getDays(currentDate, 'sunday', false, true);
+		const days = getDays(currentDate, 'sunday', false, true, false, []);
 
 		const july1 = days.find(
-			(day) => day.date.getDate() === 1 && !day.$isOutsideMonth
+			(day) => day.date.getDate() === 1 && !day.isOutsideMonth
 		);
 		if (july1) {
 			expect(july1).toEqual({
 				key: 'mocked-uuid',
 				day: 1,
 				date: new Date(2023, 6, 1),
-				$isOutsideMonth: false,
-				$holiday: true,
+				isOutsideMonth: false,
+				weekend: true,
+				holiday: false,
 			});
 		} else {
 			throw new Error('1 июля 2023 года не найден в массиве days');
 		}
 
 		const july2 = days.find(
-			(day) => day.date.getDate() === 2 && !day.$isOutsideMonth
+			(day) => day.date.getDate() === 2 && !day.isOutsideMonth
 		);
 		if (july2) {
-			expect(july2.$holiday).toBe(true);
+			expect(july2.weekend).toBe(true);
 		} else {
 			throw new Error('2 июля 2023 года не найден в массиве days');
 		}
 	});
 
 	test('возвращает правильные дни, включая дополнительные дни из предыдущего и следующего месяца, отмечая праздники, начиная с понедельника', () => {
-		const days = getDays(currentDate, 'monday', true, true);
+		const days = getDays(currentDate, 'monday', true, true, false, []);
 		expect(days).toHaveLength(42); // Отображение 6 недель
 
 		expect(days[0]).toEqual({
 			key: 'mocked-uuid',
 			day: 26,
 			date: new Date(2023, 5, 26),
-			$isOutsideMonth: true,
-			$holiday: false,
+			isOutsideMonth: true,
+			weekend: false,
+			holiday: false,
 		});
 	});
 
 	test('корректно обрабатывает крайний случай для февраля в високосный год', () => {
 		const leapYearDate = new Date(2024, 1, 1);
-		const days = getDays(leapYearDate, 'monday', false, false);
+		const days = getDays(leapYearDate, 'monday', false, false, false, []);
 		const daysInMonth = 29;
-		expect(days.filter((day) => !day.$isOutsideMonth)).toHaveLength(
-			daysInMonth
-		);
+		expect(days.filter((day) => !day.isOutsideMonth)).toHaveLength(daysInMonth);
 
 		const feb29 = days.find(
-			(day) => day.date.getDate() === 29 && !day.$isOutsideMonth
+			(day) => day.date.getDate() === 29 && !day.isOutsideMonth
 		);
 		if (feb29) {
 			expect(feb29).toEqual({
 				key: 'mocked-uuid',
 				day: 29,
 				date: new Date(2024, 1, 29),
-				$isOutsideMonth: false,
-				$holiday: false,
+				isOutsideMonth: false,
+				weekend: false,
+				holiday: false,
 			});
 		} else {
 			throw new Error('29 февраля 2024 года не найден в массиве days');
